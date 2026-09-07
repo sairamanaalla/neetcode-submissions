3931060bb9549066d81b42e3cebccbcc095fd795
sairamanaalla <66791415+sairamanaalla@users.noreply.github.com>
2026-09-07 16:32:51 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def heapify(self,nums,size,i):
+        while True:
+
+            largest = i
+
+            left = 2*i+1
+            right = 2*i+2
+
+            if left < size and nums[left]>nums[largest]:
+                largest = left
+            if right < size and nums[right] > nums[largest]:
+                largest = right
+            
+            if largest == i:
+                break
+            
+            nums[largest],nums[i] = nums[i],nums[largest]
+
+            i=largest
+            
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+
+        for i in range((n//2)-1,-1,-1):
+            self.heapify(nums,n,i)
+        
+        for i in range(n-1,0,-1):
+            nums[i],nums[0] = nums[0],nums[i]
+            self.heapify(nums,i,0)
+        
+        
